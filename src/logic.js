e25a7d56cd8446773cd952c07471bf1532fd3f17
plurryt <70047632+pythoniaweb@users.noreply.github.com>
2022-06-29 13:35:13 +0300

    Update logic.js

--- src/logic.js
+++ src/logic.js
@@ -406,71 +406,15 @@ function runLevel(level, Display, andThen) {
   });
 }
 
-function setCookie(cname,cvalue,exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-function checkCookie() {
-  let user = getCookie("level");
-  if (user != "") {
-    alert("Your level " + user);
-  } else {
-     user = prompt("Please enter your name:","");
-     if (user != "" && user != null) {
-       setCookie("level", user, 30);
-     }
-  }
-}
-
-
 function runGame(plans, Display) {
   function startLevel(n) {
    
     runLevel(new Level(plans[n]), Display, function(status) {
       if (status == "lost")
-	  d.setTime(d.getTime() + (999*24*60*60*1000));
-	  let expires = "expires=" + d.toUTCString();
-	  document.cookie = 'level' + "=" + n + ";" + expires + ";path=/";
-          startLevel(n);
+        startLevel(n);
         
       else if (n < plans.length - 1)
-	  d.setTime(d.getTime() + (999*24*60*60*1000));
-	  let expires = "expires=" + d.toUTCString();
-	  f = n + 1
-	  document.cookie = 'level' + "=" + f + ";" + expires + ";path=/";
-	  let cname = 'leve'
-	  let name = cname + "=";
-	  let decodedCookie = decodeURIComponent(document.cookie);
-	  let ca = decodedCookie.split(';');
-	  for(let i = 0; i < ca.length; i++) {
-	    let c = ca[i];
-	    while (c.charAt(0) == ' ') {
-	      c = c.substring(1);
-	    }
-	    if (c.indexOf(name) == 0) {
-	      var declevel = c.substring(name.length, c.length);
-	    }
-	  };
-          startLevel(declevel);
+        startLevel(n + 1);
       else{
 		 document.body.innerHTML='<div class="won"><h1>YOU WON !<h1></div>';
 	  }
